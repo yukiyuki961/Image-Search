a99fc17f3a13f4b4abcba8ea3df4bc44739fce00
Yuki Nabeshima <yukinabe3561@icloud.com>
2017-12-26 00:56:00 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'open-uri'
+require "sinatra/json"
+
+get '/' do
+    @imagesearch = ImageSearch.all
+    erb :index
+end
+
+get '/photo/apple' do
+  "https://kotobank.jp/image/dictionary/nipponica/media/813060240025579.jpg"
+end
+
+get '/photo/melon' do
+  "https://kotobank.jp/image/dictionary/daijisen/media/104411L.jpg"
+end
+  
